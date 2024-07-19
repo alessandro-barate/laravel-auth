faa9d5036de6c221cf9c0d4bd238d6a3fb9e4bca
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-07-19 18:56:24 +0200

    [UPD] Seeding db with specific values

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,14 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $post = new Post();
+
+        // Assegno valori specifici alle colonne
+        $post->content = 'Contenuto';
+        $post->slug = 'Slug';
+
+        // Salva il dato in tabella
+        $post->save();
+
     }
 }
