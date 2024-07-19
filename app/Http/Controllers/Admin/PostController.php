4f8483774c8ceda8ca9300d477439b082aa7aaf5
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-07-19 18:26:41 +0200

    [UPD] Post controller structured

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -14,6 +14,9 @@ class PostController extends Controller
      */
     public function index()
     {
+
+        $posts = Post::all();
+
         return view('admin.posts.index');
     }
 
@@ -22,7 +25,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.posts.create');
     }
 
     /**
@@ -30,7 +33,12 @@ class PostController extends Controller
      */
     public function store(StorePostRequest $request)
     {
-        //
+        $data = $request->validated();
+        $post = new Post();
+        $post->fill($data);
+        $post->save();
+
+        return redirect()->route('posts.show', $post->id);
     }
 
     /**
@@ -38,7 +46,11 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        if ($post === null) {
+            return redirect()->route('errors.404 - Al momento non ci sono progetti da visualizzare');
+        }
+
+        return view('posts.show', compact('post'));
     }
 
     /**
@@ -46,22 +58,35 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        $post = Post::findOrFail($id);
+
+        return view('posts.edit' , compact('post'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePostRequest $request, Post $post)
+    public function update(UpdatePostRequest $request, string $id)
     {
-        //
+        $post = Post::findOrFail($id);
+        // $data = $this->validation($request->all());
+        $data = $request->validated();
+        $post->update($data);
+        $post->title = $data['title'];
+        $post->save();
+
+        return redirect()->route('posts.show', $post->id);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Post $post)
+    public function destroy(string $id)
     {
-        //
+        $post = Post::findOrFail($id);
+
+        $post->delete();
+
+        return redirect()->route('posts.index');
     }
 }
