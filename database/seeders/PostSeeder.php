8916905d00e72c1375b58c76fc1dd69b298ed1ae
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-07-23 16:12:11 +0200

    [UPD] Seeding db with faker

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -5,22 +5,29 @@ namespace Database\Seeders;
 use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class PostSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        $post = new Post();
 
-        // Assegno valori specifici alle colonne
-        $post->content = 'Contenuto';
-        $post->slug = 'Slug';
+        for($i = 0; $i < 100; $i++) {
 
-        // Salva il dato in tabella
-        $post->save();
+            $post = new Post();
+
+            // Assegno valori specifici alle colonne
+            $post->title = $faker->sentence(3);
+            $post->content = $faker->text(500);
+            $post->slug = Str::of($post->title)->slug('-');
+
+            // Salva il dato in tabella
+            $post->save();
+        }
 
     }
 }
